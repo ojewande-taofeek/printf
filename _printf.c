34b18336d0cf2bc0431b7ed19bc57fd6c2c23219
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-06-27 07:19:29 -0700

    task 0

---
+++ _printf.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _printf - The fucntion that prints various inputs of different formats
+ * @format: The string supplied to be printed
+ * Return: len, if successful
+ */
+
+int _printf(const char *format, ...)
+{
+	int idx, len = 0;
+	char check;
+	va_list printf_arg;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(printf_arg, format);
+	for (idx = 0; format[idx] && (format[idx] != '\0'); idx++)
+	{
+		if (format[idx] == '%')
+		{
+			idx++;
+			check = format[idx];
+			if (check == 'c' || check == 's' || check == '%')
+				len += f_caller(check)(printf_arg);
+		}
+		else
+		{
+			write(1, &format[idx], 1);
+			len++;
+		}
+	}
+	va_end(printf_arg);
+	return (len);
+}
---
+++ printf_functions.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+int put_char(va_list printf_arg);
+int put_str(va_list printf_arg);
+int put_mod(va_list printf_arg);
+int put_digits(va_list printf_arg);
+
+/**
+ * put_char - The function that prints characters
+ * @printf_arg: The va_list
+ * Return: Each character
+ */
+
+int put_char(va_list printf_arg)
+{
+	char c;
+
+	c = va_arg(printf_arg, int);
+	write(1, &c, 1);
+	return (1);
+}
+
+
+/**
+ * put_str - The function that prints string
+ * @printf_arg: The va_list
+ * Return: The string length
+ */
+
+int put_str(va_list printf_arg)
+{
+	char *str;
+	int idx = 0, len = 0;
+
+	str = va_arg(printf_arg, char *);
+	if (str == NULL)
+		str = "(null)";
+	while (str[idx])
+	{
+		write(1, &str[idx], 1);
+		len++;
+		idx++;
+	}
+	return (len);
+}
+
+/**
+ * put_mod - The function that prins percentage '%'
+ * @printf_arg: The va_list
+ * Return: 1
+ */
+
+int put_mod(va_list printf_arg)
+{
+	char m;
+
+	UNUSED(printf_arg);
+	m = '%';
+	write(1, &m, 1);
+	return (1);
+}
